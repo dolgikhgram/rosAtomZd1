2841dc576c3e99125a5056a90ec2f348987f8e4c
User <user@MacBook-Air-User.local>
2024-10-11 21:54:08 +0300

    second commit

--- src/components /Content/Content.jsx
+++ src/components /Content/Content.jsx
@@ -6,16 +6,30 @@ import { Box, Button,Container, Grid, Typography } from "@mui/material";
 const Content = ()=>{
     let colors = ["#14cc8d", "#1481cc", "#cc3114", "#bb14cc", "#14ccbb", "#5f14cc", "#cc8d14"]
     const url = 'https://api.forismatic.com/api/1.0/?method=getQuote&amp;key=random&amp;format=jsonp&amp;lang=ru&amp;jsonp=?'
-    fetch("https://api.forismatic.com/api/1.0/?method=getQuote&key=random&format=jsonp&lang=ru&jsonp=?").then((r)=>{
-        console.log(r.text().then(data=>console.log(data)))
-    })
+    // fetch(url)
+    // .then((r)=>r.text())
+    // .then(data=>console.log(data))
     
     const [color, setColor] = useState(1)
-    const [quotes,setQuotes] = useState()
+    const [quotes,setQuotes] = useState('')
     const handleClick = ()=>{
         let el = Math.floor(Math.random()*8)
         setColor(el)
+        fetch(url)
+        .then((r)=>r.text())
+        .then(data=>setQuotes(data))
     }
+    let text = ''
+    let author = ''
+    if (quotes.length>0){
+        let startAuthor = quotes.indexOf('<quoteAuthor>')+13
+        let endAuthor = quotes.indexOf('</quoteAuthor>')
+        let endText = quotes.indexOf('</quoteText>')
+        console.log(quotes)
+        console.log(startAuthor)
+        text = quotes.slice(30,endText)
+        author=quotes.slice(startAuthor,endAuthor)
+    }    
     return (
         <div className={s.content} style={{
             backgroundColor: colors[color],
@@ -28,14 +42,21 @@ const Content = ()=>{
                         </Container>
                     </Grid>
                 </Grid>
-                <Grid container justifyContent='center'>
+                <Grid container justifyContent='center' spacing={2}>
                     <Grid item>
                         <Container >
-                            <Typography></Typography>
+                            <Typography variant="h6">{text}</Typography>
                         </Container>
                     </Grid>
                 </Grid>
-                <Grid container justifyContent='center'>
+                <Grid container justifyContent='center' spacing={2}>
+                    <Grid item>
+                        <Container >
+                            <Typography variant="h6">{author}</Typography>
+                        </Container>
+                    </Grid>
+                </Grid>
+                <Grid container justifyContent='center' spacing={2}>
                     <Grid item>
                         <Box>
                             <Button variant="contained" onClick={handleClick}>Новая цитата</Button>
